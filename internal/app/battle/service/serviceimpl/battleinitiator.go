691373d37f6939f32e8323dc9c9fbc006ac325e8
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-02-19 12:08:59 +0700

    refactor: decrease BattleInitiator.Initiate complexity

--- internal/app/battle/service/serviceimpl/battleinitiator.go
+++ internal/app/battle/service/serviceimpl/battleinitiator.go
@@ -22,68 +22,64 @@ func NewBattleInitiator(opts *BattleInitiatorOptions) *BattleInitiator {
 	return &BattleInitiator{opts: opts}
 }
 //Note: this code is bad code, this is for codescene bad code scanning testing only
-//TODO: refactor this shit
+
 func (b BattleInitiator) Initiate(ctx context.Context, command service.InitiateBattleCommand) (successEvent service.BattleInitiatedEvent, err error) {
 	var battleCharacters []entity.BattleCharacter
 	//Add Left side Character
-	leftCharaIds := command.LeftSideCharactersIDs
-	var leftCharacters []*entity.Character
-
-	for _,charaId := range leftCharaIds {
-		chara,err := b.opts.CharacterRepository.LoadByID(ctx,charaId)
-		if err != nil {
-			return successEvent,err
-		}
-		leftCharacters = append(leftCharacters,chara)
+	leftSideCharacters,err := b.findCharactersWhereIdIn(ctx,command.LeftSideCharactersIDs)
+	if err != nil {
+		return successEvent,err
 	}
-
-	for _,c := range leftCharacters {
-		leftBattleCharacter := entity.BattleCharacter{
-			CharacterID:     c.ID,
-			MaxMP:           c.Stat.BaseHP,
-			MP:              c.Stat.BaseMP,
-			HP:              c.State.CurrentHP,
-			MaxHP:           c.State.CurrentMP,
-			Side:            battle.SideLeft,
-		}
-		battleCharacters = append(battleCharacters,leftBattleCharacter)
+	leftSideBattleCharacters := b.mapCharactersToBattleCharacters(ctx,leftSideCharacters,battle.SideLeft)
+	battleCharacters = append(battleCharacters,leftSideBattleCharacters...)
+	//Add Right side Character
+	rightSideCharacters,err := b.findCharactersWhereIdIn(ctx,command.RightSideCharactersIDs)
+	if err != nil {
+		return successEvent,err
 	}
-
-	//Add Left side Character
-	rightCharaIds := command.RightSideCharactersIDs
-	var rightCharacters []*entity.Character
-	for _,charaId := range rightCharaIds {
-		chara,err := b.opts.CharacterRepository.LoadByID(ctx,charaId)
-		if err != nil {
-			return successEvent,err
-		}
-		rightCharacters = append(rightCharacters,chara)
-	}
-
-	for _,c := range rightCharacters {
-		rightBattleCharacter := entity.BattleCharacter{
-			CharacterID:     c.ID,
-			MaxMP:           c.Stat.BaseHP,
-			MP:              c.Stat.BaseMP,
-			HP:              c.State.CurrentHP,
-			MaxHP:           c.State.CurrentMP,
-			Side:            battle.SideRight,
-		}
-		battleCharacters = append(battleCharacters,rightBattleCharacter)
-	}
+	rightSideBattleCharacters := b.mapCharactersToBattleCharacters(ctx,rightSideCharacters,battle.SideRight)
+	battleCharacters = append(battleCharacters,rightSideBattleCharacters...)
 	//Create battle session
 	session := entity.BattleSession{
 		Characters: battleCharacters,
 	}
 
-	err = b.opts.BattleRepository.CreateOrUpdate(ctx,&session)
+	err = b.opts.BattleRepository.CreateOrUpdate(ctx, &session)
 	if err != nil {
-		return service.BattleInitiatedEvent{},err
+		return service.BattleInitiatedEvent{}, err
 	}
 
 	return service.BattleInitiatedEvent{
 		SessionID:             session.ID,
 		LeftSideCharacterIDs:  command.LeftSideCharactersIDs,
 		RightSideCharacterIDs: command.RightSideCharactersIDs,
-	},nil
+	}, nil
+}
+
+func (b BattleInitiator) mapCharactersToBattleCharacters(ctx context.Context, characters []*entity.Character,side entity.BattleSide) (result []entity.BattleCharacter) {
+	for _, c := range characters {
+		b := entity.BattleCharacter{
+			CharacterID: c.ID,
+			MaxMP:       c.Stat.BaseHP,
+			MP:          c.Stat.BaseMP,
+			HP:          c.State.CurrentHP,
+			MaxHP:       c.State.CurrentMP,
+			Side:        side,
+		}
+		result = append(result, b)
+	}
+	return result
+}
+//TODO: refactor this shit, move to repository or somewhere else has this responsibility
+func (b BattleInitiator) findCharactersWhereIdIn(ctx context.Context,characterIds []uint) ([]*entity.Character,error) {
+	var characters []*entity.Character
+
+	for _, charaId := range characterIds {
+		chara, err := b.opts.CharacterRepository.LoadByID(ctx, charaId)
+		if err != nil {
+			return nil,err
+		}
+		characters = append(characters,chara)
+	}
+	return characters,nil
 }
